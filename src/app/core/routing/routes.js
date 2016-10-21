01b004af9168807608b9eaae91e463d6cc161681
Holt Mansfield <holt.mansfield@mobilelive.ca>
2016-10-21 17:06:17 -0400

    mege

--- src/app/core/routing/routes.js
+++ src/app/core/routing/routes.js
@@ -1,7 +1,7 @@
 "use strict";
 var home_component_1 = require("../home/home.component");
-var login_component_ts_1 = require("../user/login.component.ts");
-var contact_component_ts_1 = require("../contact/contact.component.ts");
+var login_component_1 = require("../user/login.component");
+var contact_component_1 = require("../contact/contact.component");
 exports.AppRoutes = [
     {
         path: '',
@@ -13,7 +13,7 @@ exports.AppRoutes = [
     },
     {
         path: 'login',
-        component: login_component_ts_1.LoginComponent
+        component: login_component_1.LoginComponent
     },
     {
         path: 'about',
@@ -25,7 +25,7 @@ exports.AppRoutes = [
     },
     {
         path: 'contact',
-        component: contact_component_ts_1.ContactComponent
+        component: contact_component_1.ContactComponent
     },
     {
         path: 'lcr',
